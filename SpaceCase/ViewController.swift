89336fd3336a7946f7afdc0a2c6ea31a7bfc8fd3
cjbarth1 <cjbarth1@gmail.com>
2018-02-10 19:25:50 -0500

    Initial Commit

---
+++ SpaceCase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceCase
+//
+//  Created by Casey Barth on 2/10/18.
+//  Copyright © 2018 Casey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
